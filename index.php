ca2605fa90fe57a748bcd90ba775a89c5336edb5
mohammed <mohammedemail@gmail.com>
2025-08-11 00:18:08 +0300

    Create file index.php & index.css

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OnlineStore</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <center>
+        
+        <div class="mine">
+            <form action="insert.php" id="ga" method="post">
+                <h2>My Shope</h2>
+                <img src="White Typography Initial BH Logo.png" alt="logo" width="60%" height="100%">
+                <br>
+                <input type="text" name="name">
+                <br>
+                <input type="pass" name="pirce">
+                <br>
+                <input type="file" id="file" name="image" style="display:none">
+                <label for="file">إرفع المنتج</label>
+            </form>
+            <a href="">تصفح جميع المنتجات</a>
+        </div>
+    </center>
+</body>
+</html>
