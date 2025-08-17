6fe8836dcb89ede936e66334b1a3ffa7e5c5b713
mohammed <mohammedemail@gmail.com>
2025-08-17 21:33:56 +0300

    Create files index.php & index.css

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cabin:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OnlineStore</title>
@@ -9,20 +12,24 @@
 <body>
     <center>
         
-        <div class="mine">
-            <form action="insert.php" id="ga" method="post">
+        <div class="maine">
+            <form action="insert.php"  method="post">
                 <h2>My Shope</h2>
                 <img src="White Typography Initial BH Logo.png" alt="logo" width="60%" height="100%">
                 <br>
                 <input type="text" name="name">
                 <br>
-                <input type="pass" name="pirce">
+                <input type="text" name="pirce">
                 <br>
                 <input type="file" id="file" name="image" style="display:none">
-                <label for="file">إرفع المنتج</label>
+                <label for="file">إرفع صوره المنتج</label>
+                <button name="upload">رفع المنتج</button>
+                <br><br>
+                <a href="products.php">تصفح جميع المنتجات</a>
             </form>
-            <a href="">تصفح جميع المنتجات</a>
+            
         </div>
+        <p> Developer By Mohammed </p>
     </center>
 </body>
 </html>
